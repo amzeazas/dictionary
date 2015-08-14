a8b29b711ac1ce1b7bc13cb10237db7beb72b2bf
Alexa <amzeazas@comcast.net>
2015-08-14 09:55:45 -0700

    Add spec for definitions method to Word class.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -35,4 +35,10 @@ describe(Word) do
       expect(test_word.id()).to(eq(1))
     end
   end
+  description("#definitions") do
+    it("initially returns an empty array of definition for the word") do
+      test_word = Word.new(:name => "Harley Quinn")
+      expect(test_word.definitions()).to(eq([]))
+    end
+  end
 end
