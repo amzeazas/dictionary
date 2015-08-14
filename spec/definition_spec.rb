ca0e77366167d9eab8337efafe17562507ccc346
Alexa <amzeazas@comcast.net>
2015-08-14 11:41:18 -0700

    Correct formatting errors.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -5,10 +5,10 @@ describe(Definition) do
   before() do
     Definition.clear()
   end
-  
+
   describe("#descrition") do
     it("returns the description of a definition") do
-      test_definition = Definition.new(:description => "the name of my cat")
+      test_definition = Definition.new({:description => "the name of my cat"})
       expect(test_definition.description()).to(eq("the name of my cat"))
     end
   end
@@ -19,21 +19,21 @@ describe(Definition) do
   end
   describe("#save") do
     it("adds a description to the array of saved descriptions") do
-      test_definition = Definition.new(:description => "the name of my cat")
+      test_definition = Definition.new({:description => "the name of my cat"})
       test_definition.save()
       expect(Definition.all()).to(eq([test_definition]))
     end
   end
   describe(".clear") do
     it("empties the array of saved descriptions") do
-      test_definition = Definition.new(:description => "the name of my cat").save()
+      test_definition = Definition.new({:description => "the name of my cat"}).save()
       Definition.clear()
       expect(Definition.all()).to(eq([]))
     end
   end
   describe("id") do
     it("returns the id of the vehicle") do
-      test_definition = Definition.new(:description => "the name of my cat")
+      test_definition = Definition.new({:description => "the name of my cat"})
       test_definition.save()
       expect(test_definition.id()).to(eq(1))
     end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -9,7 +9,7 @@ describe(Word) do
 
   describe("#name") do
     it("returns the name of the word") do
-      test_word = Word.new(:name => "Harley Quinn")
+      test_word = Word.new({:name => "Harley Quinn"})
       expect(test_word.name()).to(eq("Harley Quinn"))
     end
   end
@@ -20,44 +20,44 @@ describe(Word) do
   end
   describe("#save") do
     it("adds a word to the array of saved words") do
-      test_word = Word.new(:name => "Harley Quinn")
+      test_word = Word.new({:name => "Harley Quinn"})
       test_word.save()
       expect(Word.all()).to(eq([test_word]))
     end
   end
   describe(".clear") do
     it("empties out all of the saved words") do
-      Word.new(:name => "Harley Quinn").save()
+      Word.new({:name => "Harley Quinn"}).save()
       Word.clear()
       expect(Word.all()).to(eq([]))
     end
   end
   describe("#id") do
     it("returns the id of the word") do
-      test_word = Word.new(:name => "Harley Quinn")
+      test_word = Word.new({:name => "Harley Quinn"})
       test_word.save()
       expect(test_word.id()).to(eq(1))
     end
   end
   describe("#definitions") do
     it("initially returns an empty array of definition for the word") do
-      test_word = Word.new(:name => "Harley Quinn")
+      test_word = Word.new({:name => "Harley Quinn"})
       expect(test_word.definitions()).to(eq([]))
     end
   end
   describe("#add_definition") do
     it("adds a new definition to a word") do
-      test_word = Word.new(:name => "Harley Quinn")
-      test_definition = Definition.new(:description => "the name of my cat")
+      test_word = Word.new({:name => "Harley Quinn"})
+      test_definition = Definition.new({:description => "the name of my cat"})
       test_word.add_definition(test_definition)
       expect(test_word.definitions()).to(eq([test_definition]))
     end
   end
   describe(".find") do
     it("returns a word by its id number") do
-      test_word = Word.new(:name => "Harley Quinn")
+      test_word = Word.new({:name => "Harley Quinn"})
       test_word.save()
-      test_word2 = Word.new(:name => "rogue")
+      test_word2 = Word.new({:name => "rogue"})
       test_word2.save()
       expect(Word.find(test_word.id())).to(eq(test_word))
     end
