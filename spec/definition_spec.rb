6446da6a48304ea7d6437be4d9a351e2f3090fd5
Alexa <amzeazas@comcast.net>
2015-08-14 09:15:16 -0700

    Add spec for description method of Definition class.

---
+++ spec/definition_spec.rb
@@ -0,0 +1,11 @@
+require('rspec')
+require('definition')
+
+describe(Definition) do
+  describe("#descrition") do
+    it("returns the description of a definition") do
+      test_definition = Definition.new(:description => "the name of my cat")
+      expect(test_definition.description()).to(eq("the name of my cat"))
+    end
+  end
+end
