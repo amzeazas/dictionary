fe89192336ec7cce159292ae231e0baedf572b58
Alexa <amzeazas@comcast.net>
2015-08-14 09:50:22 -0700

    Add id method to Word class.

--- lib/word.rb
+++ lib/word.rb
@@ -4,6 +4,7 @@ class Word
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
+    @id = @@words.length().+(1)
   end
 
   define_method(:name) do
@@ -20,5 +21,9 @@ class Word
 
   define_singleton_method(:clear) do
     @@words = []
+  end
+
+  define_method(:id) do
+    @id
   end
 end
